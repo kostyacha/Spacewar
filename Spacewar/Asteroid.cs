2797b89b69529b401326183af052b40cae3fe645
kostyacha <KostyaCh2010@hotmail.com>
2025-02-05 20:40:36 +0400

    abgd
    ura

---
+++ Spacewar/Asteroid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Spacewar
+{
+     public  class Asteroid
+    {
+        private Vector2 _position;
+        private Texture2D _texture;
+         public Asteroid() : this(Vector2.Zero)
+        {
+            
+
+
+        }
+
+        public Asteroid(Vector2 position)
+        {
+            _texture = null;
+            _position = position;
+        }
+        
+            
+
+        public void LoadContent(ContentManager Content)
+        {
+            _texture = Content.Load<Texture2D>("asteroid");
+        }
+        public void Update()
+        {
+            _position.Y += 10;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _position, Color.White);
+        }
+    }
+}
--- Spacewar/Game1.cs
+++ Spacewar/Game1.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Spacewar.Classes;
 
 namespace Spacewar
 {
+
+   
     public class Game1 : Game
     {
         //  инструменты 
@@ -12,9 +15,11 @@ namespace Spacewar
         private SpriteBatch _spriteBatch;
 
         //поля 
-
+      //  private Asteroid _asteroid;
         private Player _player;
         private Space _space;
+        
+        private List<Asteroid> _asteroids;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -25,8 +30,11 @@ namespace Spacewar
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+           // _asteroid = new Asteroid();
+            _asteroids = new List<Asteroid>();
             _player = new Player();
             _space = new Space();
+
 
             base.Initialize();
 
@@ -36,8 +44,18 @@ namespace Spacewar
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            //_asteroids.LoadContent(Content);
             _player.LoadContent(Content);
             _space.LoadContent(Content);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Asteroid asteroid = new Asteroid(new Vector2(i * 40, 0));
+                asteroid.LoadContent(Content);
+                _asteroids.Add(asteroid);
+            }
+
+
 
 
             // TODO: use this.Content to load your game content here
@@ -52,6 +70,11 @@ namespace Spacewar
             _player.Update(_graphics.PreferredBackBufferWidth,
                 _graphics.PreferredBackBufferHeight);
             _space.Update();
+            //_asteroids.Update();
+            foreach (Asteroid asteroid in _asteroids)
+            {
+                asteroid.Update();
+            }
 
             base.Update(gameTime);
         }
@@ -61,9 +84,19 @@ namespace Spacewar
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin();
-            _space.Draw(_spriteBatch);
+            {
+                _space.Draw(_spriteBatch);
+                _player.Draw(_spriteBatch);
+               // _asteroids.Draw(_spriteBatch);
 
-            _player.Draw(_spriteBatch);
+                foreach(Asteroid asteroid in _asteroids)
+                {
+                    asteroid.Draw(_spriteBatch);
+                }
+
+
+            }
+
 
             _spriteBatch.End();
 
